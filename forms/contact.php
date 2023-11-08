ee0e955f8b95a81eea948d9fe1fbeaf8c3035217
TheYaminak <joasdiaz98@hotmail.com>
2023-11-08 05:26:20 -0300

    Change email method

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,34 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'joasdiaz98@gmail.com';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  $name = trim($_POST["name"]);
+  $email = trim($_POST["email"]);
+  $subject = trim($_POST["subject"]);
+  $message = trim($_POST["message"]);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+  $smtp = array(
+    'host' => 'smtp-mail.outlook.com',
+    'username' => 'joasdiaz98@hotmail.com',
+    'password' => 'theyaminak98',
+    'port' => 587,
   );
-  */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  $to = 'joasdiaz98@gmail.com';
+
+  $email_message = "Name: $name\n";
+  $email_message .= "Email: $email\n";
+  $email_message .= "Subject: $subject\n";
+  $email_message .= "Message:\n$message";
+
+  $headers = "From: $email\n";
+  $headers .= "Reply-To: $email\n";
+
+  if (mail($to, $subject, $email_message, $headers)) {
+    echo 'OK';
+  } else {
+    echo 'Error al enviar el correo';
+  }
+}
 ?>
